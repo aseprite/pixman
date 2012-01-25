c5c866a394c823b2980eff15ce33ce8356010bbe
Søren Sandmann Pedersen <ssp@redhat.com>
2012-01-25 14:03:05 -0500

    Revert "Reject trapezoids where top (botttom) is above (below) the edges"
    Cairo 1.10 will sometimes generate trapezoids like this, so we can't
    consider them invalid. Fixes bug 45009, reported by Michael Biebl.
    
    This reverts commit 2437ae80e5066dec9fe52f56b016bf136d7cea06.

--- test/composite-traps-test.c
+++ test/composite-traps-test.c
@@ -252,6 +252,6 @@ test_composite (int      testnum,
 int
 main (int argc, const char *argv[])
 {
-    return fuzzer_test_main("composite traps", 40000, 0x4346479C,
+    return fuzzer_test_main("composite traps", 40000, 0xE3112106,
 			    test_composite, argc, argv);
 }
